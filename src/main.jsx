f3bca15ea1260d77d11ce73878db15e81d93e1f7
dimadimadima1 <dddimacut@gmail.com>
2026-06-18 21:20:37 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CTABuilder from "../CTABuilder.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CTABuilder />
+  </React.StrictMode>
+);
