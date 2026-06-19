8d62c6ed29d28a48dbcc19bf6ab908745f25fa52
dimadimadima1 <dddimacut@gmail.com>
2026-06-19 12:04:25 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import CTADirector from "./CTABuilder.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CTADirector />
-  </React.StrictMode>
-);
